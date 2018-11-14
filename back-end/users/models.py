1cbe92f1c69b2a75338502eebac1b1f9f8232d57
“khalifahalsadah” <khalifa.kh.as@gmail.com>
2018-11-14 21:04:38 +0300

    added user class

---
+++ back-end/users/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.functional import cached_property
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=email,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    nick_name = models.CharField(max_length=150, blank=False)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True, blank=True, null=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    registration_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ back-end/users/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+
+# Create your views here.
+
+
