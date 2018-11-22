93ccb5a7cce4c8d43e094ce25ea553c474db4583
Meshal <s201336290@kfupm.edu.sa>
2018-11-22 04:44:47 +0300

    Add Enter sission UI

--- android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/MainActivity.kt
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/MainActivity.kt
@@ -1,14 +1,21 @@
 package example.kfupm.moyoon.moyoon
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val join = findViewById<Button>(R.id.join)
+        join.setOnClickListener{
+            val intent = Intent(this,Sission_Creation::class.java)
+            startActivity(intent)
+        }
     }
-    // This is a test for Editing :)
 }
 
