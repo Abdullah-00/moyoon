273792a74ade6d1cb5f823fa24a5b9a6464ca0f6
Bandar <bandar@bandars-mac-pro.local>
2018-11-30 19:23:59 +0300

    firebase init code

--- ios/Moyoon/Moyoon/AppDelegate.swift
+++ ios/Moyoon/Moyoon/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
