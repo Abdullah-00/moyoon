fd4339ce8ddbaee8d54c37260d449b71ea1e08bb
RayanIJ <44984356+RayanIJ@users.noreply.github.com>
2018-11-30 17:44:29 +0300

    Add files via upload

---
+++ Moyoon/Moyoon/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Moyoon
+//
+//  Created by Bandar on 16/11/2018.
+//  Copyright © 2018 KFUPM-SWE417. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    @IBAction func play(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
