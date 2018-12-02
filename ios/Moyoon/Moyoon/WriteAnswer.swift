2c09e048b59a7faf85047218a98c53a15b995d77
Bandar <bandar@bandars-mac-pro.local>
2018-12-02 20:15:52 +0300

    added send answer to API

--- ios/Moyoon/Moyoon/WriteAnswer.swift
+++ ios/Moyoon/Moyoon/WriteAnswer.swift
@@ -16,9 +16,32 @@ class WriteAnswer: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBOutlet weak var answerField: UITextField!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func submitAnswer(_ sender: Any) {
+        sendAnswerToServer(answerField.text)
+    }
+    
+    func sendAnswerToServer(answer: String){
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "http"
+        urlComponents.host = GlobalVariables.hostname
+        urlComponents.path = "/SubmitAnswer/"
+        let player_id = URLQueryItem(name: "player_id", value: "\(GlobalVariables.playerId)")
+        let session_id = URLQueryItem(name: "session_id", value: "\(GlobalVariables.sessionId)")
+        let round_id = URLQueryItem(name: "round_id", value: "\(GlobalVariables.roundId)")
+        let question_id = URLQueryItem(name: "question_id", value: "\(GlobalVariables.questionId)")
+        let answer = URLQueryItem(name: "answer", value: "\(answer)")
+
+        urlComponents.queryItems = [player_id,session_id,round_id,question_id,answer]
+        guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+    }
 }
