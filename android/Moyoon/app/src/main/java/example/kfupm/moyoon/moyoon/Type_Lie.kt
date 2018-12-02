7c5210c4e3ab8b7f959bca4eff36a382207956ba
Ibraheem <ibrafayez@gmail.com>
2018-12-02 21:03:34 +0300

    Firestore V0.6

--- android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Type_Lie.kt
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Type_Lie.kt
@@ -5,23 +5,27 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import java.util.*
 
 
-class Type_Lie : AppCompatActivity() {
+abstract class Type_Lie : AppCompatActivity() {
 
     lateinit var question_desplay : TextView
     lateinit var lie : EditText
     lateinit var Submit_lie : Button
+    lateinit var db : FirebaseFirestore
 
-    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.type_lie)
 
+        val TAG = "DocSnippets"
+        db = FirebaseFirestore.getInstance()
 
         question_desplay = findViewById<TextView>(R.id.question_desplay)
         lie = findViewById(R.id.Lie)
@@ -36,6 +40,20 @@ class Type_Lie : AppCompatActivity() {
            /* val intent = Intent(this,Display_Answers::class.java)
             startActivity(intent)*/
         }
+    }
+    private fun getQuestion() {
+        // [START get_all_users]
+        db.collection("Session/8zdNKG1g8VrCuDCeRTds/Rounds/1/Questions/1").get()
+            .addOnSuccessListener { result ->
+            for (document in result) {
+
+                Log.d("DocSnippets", document.id + " => " + document.data)
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.w("DocSnippets", "Error getting documents.", exception)
+            }
+        // [END get_all_users]
     }
 
     private fun saveLie(){
