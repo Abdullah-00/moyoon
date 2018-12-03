d7a7683b5fd1e7ce4e6313a3cbfce24d952f082f
Ibraheem <ibrafayez@gmail.com>
2018-12-03 03:09:06 +0300

    mesh

---
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Global.kt
@@ -0,0 +1,9 @@
+package example.kfupm.moyoon.moyoon
+
+
+class Global{
+    var sessionID: String ? = null
+    var roundID : ArrayList<String> ?= null
+    var questionID : ArrayList<String> ?= null
+    var playerID : String ?= null
+}
