78a1de0e37f8c05ee4aa32f6eae77edabe38d80a
Bandar <bandar@Bandars-Mac-Pro.local>
2018-12-05 22:38:26 +0300

    FirebaseUI

--- ios/Moyoon/Moyoon/Lobby.swift
+++ ios/Moyoon/Moyoon/Lobby.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseCore
 import FirebaseFirestore
+import FirebaseUI
+
 
 class Lobby: UIViewController {
     
@@ -20,6 +23,42 @@ class Lobby: UIViewController {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let db = Firestore.firestore()
+        let query = db.collection("/Session/052074/Players").document("1lov5jxfeCSZE4Uzk0i1")
+            .addSnapshotListener { documentSnapshot, error in
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    return
+                }
+                guard let data = document.data() else {
+                    print("Document data was empty.")
+                    return
+                }
+                print("Current data: \(data)")
+        }
+
+        
+
+        
+        let dataSource = self.Players.bind(toFirestoreQuery: query as! Query, populateCell: { (tableView: UITableView, indexPath: IndexPath, snapshot: DocumentSnapshot) -> UITableViewCell in
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+            
+            let value = snapshot.data() as! NSDictionary
+            
+            let someProp = value["Score"] as? String ?? ""
+            
+            cell.textLabel?.text = someProp
+            
+            return cell
+        })
     }
 
     @IBOutlet weak var Players: UITableView!
