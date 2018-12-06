6ed2ca960f30143ff3ab9c56851b4dde33c05b43
Meshal <s201336290@kfupm.edu.sa>
2018-12-06 04:52:26 +0300

    Add  Finish Game UI

---
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/EndOfGame.kt
@@ -0,0 +1,12 @@
+package example.kfupm.moyoon.moyoon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EndOfGame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_of_game)
+    }
+}
--- android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Global.kt
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Global.kt
@@ -4,7 +4,7 @@ package example.kfupm.moyoon.moyoon
 class Global{
     companion object {
 
-        var sessionID: String = "186232"
+        var sessionID: String = "390103"
         var roundID: ArrayList<String> = ArrayList()
         var roundNum : Int = 0
         var questionID: ArrayList<String> = ArrayList()
