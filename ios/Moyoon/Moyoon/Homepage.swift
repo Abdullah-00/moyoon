3a048f82f8c76cb1b7355216384db539e179b251
Rayan <Rayan@imac.local>
2019-02-04 18:16:01 +0300

    temp logo for now

--- ios/Moyoon/Moyoon/Homepage.swift
+++ ios/Moyoon/Moyoon/Homepage.swift
@@ -22,6 +22,8 @@ class Homepage: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
