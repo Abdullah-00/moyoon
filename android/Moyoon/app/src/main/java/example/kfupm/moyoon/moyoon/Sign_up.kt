7ce4084c075d04b44d0b8639a1636479e1fdf07f
Ibraheem <ibrafayez@gmail.com>
2019-04-13 01:08:56 +0300

    Android: Text and buttons are changed

--- android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Sign_up.kt
+++ android/Moyoon/app/src/main/java/example/kfupm/moyoon/moyoon/Sign_up.kt
@@ -3,31 +3,33 @@ package example.kfupm.moyoon.moyoon
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class Sign_up : AppCompatActivity() {
 
     // lateinit var guest: ImageButton
-     lateinit var google: ImageButton
-    lateinit var FB: ImageButton
+     lateinit var google: Button
+    lateinit var FB: Button
+    lateinit var backHome : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-//     guest = findViewById(R.id.button)
         google = findViewById(R.id.google)
         FB = findViewById(R.id.FB)
+        backHome = findViewById(R.id.backHome)
 
-     //   val mainActiv = Intent(this, MainActivity::class.java)
         val googleSign = Intent(this, Sign_in_google::class.java)
         val FBsignin = Intent(this, Sign_in_Facebook::class.java)
+        val back = Intent(this, MainActivity::class.java)
 
 
-//        guest.setOnClickListener {
-//            startActivity(mainActiv)
-//        }
-            google.setOnClickListener {
+        backHome.setOnClickListener {
+            startActivity(back)
+        }
+        google.setOnClickListener {
                 startActivity(googleSign)
         }
         FB.setOnClickListener {
